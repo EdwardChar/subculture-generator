fa6988247b27316933e1eb9950b7d8da8b955e8e
edwardchar <edwardchar@yahoo.com>
2021-08-06 03:28:02 -0400

    add actual files

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# subculture-generator
+（致读者，包括潜在用户以及点进我github的招聘人员：这是一个娱乐项目。请忽略readme中不严谨的部分）
+(To readers, including potential users and recruiters: This project is merely for fun. Please ignore anything that doesn't sound professional.)
+# 亚文生成器2022: Complete Nonsense
+还在往微信朋友圈转发谣言？拿手机刷三手新闻？看微博机器人大战？Pffft。
+
+美国克莱登大学沃兹·基硕德的研究表明，99%的微博用户发言其实都是机器人发的。既然如此，为什么不直接去看机器人写的文章呢？
+
+隆重为您介绍：
+
+## 亚文生成器
+
+包含：
++ 从网络上（新浪新闻）随机扒下来的词语
++ 互联网最唉的资本家：埃隆·马斯克
++ 胡言乱语
++ Complete Nonsense
++ 华为
+
+## How 2
+
+### Dependencies
+
+[newspaper3k](https://pypi.org/project/newspaper/), [jieba](https://pypi.org/project/jieba/). Install them before running the script!
+
+### Run
+
+    python generate.py
+
+## TODO
+
+    使用命令行参数修改各参数
+
---
+++ generate.py
@@ -0,0 +1,66 @@
+import newspaper, random, time
+from newspaper import Config, Article
+import jieba
+
+def cult():
+    # crawl 200 articles
+    paper = newspaper.build("http://tech.sina.com.cn", language="zh")
+    paragraph = []
+    config = Config()
+    config.request_timeout = 3
+    for article in paper.articles[:50]:
+        try:
+            good_article = Article(url = article.url.rstrip("\r\n").rstrip(" "), config = config, language='zh')
+            good_article.download()
+            good_article.parse()
+            if len(good_article.text):
+                paragraph.extend(good_article.text)
+        except Exception as err:
+                print("Encountered exception. Automatically skips this article. {}".format(err))
+        finally:
+            time.sleep(0.1)
+    raw = "".join(paragraph)
+    lr = list(jieba.cut(raw))
+    ln = random.choices(lr,k = 2000)
+    # add names to make it funnier
+    ln.extend(["马斯克","马云","扎克伯格","马化腾","比尔盖茨","Facebook","阿里巴巴","贝索斯","库克","苹果","小米","华为","5G"])
+    random.shuffle(ln)
+    naw = ""
+    for i, word in enumerate(ln):
+        r = random.random()
+        
+        if r < 0.02:
+            naw += "。|"
+        elif r < 0.06:
+            naw += "，"
+        naw += word
+    if not naw.endswith("。|"):
+        naw += "。"
+    sent = naw.split("|")
+    abstract = "编者按："
+    pg1 = ""
+    pg2 = ""
+    pg3 = ""
+    concl = "结语："
+    # create "abstract"
+    for i in range(5):
+        abstract += sent.pop()
+    abstract += "\n\n"
+    
+    for i in range(3):
+        concl += sent.pop()
+    concl += "\n"
+    perp = len(sent)//4+1
+    for i in range(perp):
+        pg1 += sent.pop()
+        pg2 += sent.pop()
+        pg3 += sent.pop()
+    pg1 += "\n\n"
+    pg2 += "\n\n"
+    pg3 += "\n\n"    
+    pg4 = "".join(sent)+"\n\n"
+    passa = abstract + pg1 + pg2 + pg3 + pg4 + concl
+    return passa
+
+with open("subculture_revived.txt","w",newline="", encoding="utf-8") as cult_file:
+    cult_file.write(cult())
